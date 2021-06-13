88f6db1e123ab546c41f8751dafa1a226a03f388
Resminos <loic75019@hotmail.fr>
2021-06-13 23:27:10 +0200

    Add files via upload

---
+++ lockdownpet/lib/src/screens/login.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:lockdownpet/main.dart';
+import 'package:lockdownpet/src/screens/profil.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late String _email, _password;
+  final auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Login'),),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: 'Email'
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _email = value.trim();
+                });
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Password'),
+              onChanged: (value) {
+                setState(() {
+                  _password = value.trim();
+                });
+              },
+            ),
+
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children:[
+                RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text('Signin'),
+                    onPressed: (){
+                      auth.signInWithEmailAndPassword(email: _email, password: _password).then((_){
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyApp()));
+                      });
+
+                    }),
+                RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Signup'),
+                  onPressed: (){
+                    auth.createUserWithEmailAndPassword(email: _email, password: _password).then((value){
+                      FirebaseFirestore.instance.collection('UserData').doc(value.user!.uid).set({"email": value.user!.email});
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyApp()));
+                    });
+
+                  },
+                )
+              ])
+        ],),
+    );
+  }
+}
